788b16ffca24e8b718de23059ec163a9d0fc8e06
dafengshui <90216512+dafengshui@users.noreply.github.com>
2021-09-06 14:47:36 -0500

    Update data.js
    All-Time Summer Jams

--- static/js/data.js
+++ static/js/data.js
@@ -1,27 +1,7 @@
 var playlists = [
     {
-      "name": "most-viewed",
-      "id": "PL15B1E77BB5708555",
-      "max": 543
-    },
-    {
-      "name": "billboard",
-      "id": "PL55713C70BA91BD6E",
-      "max": 200
-    },
-    {
-      "name": "latest",
-      "id": "PLFgquLnL59akA2PflFpeQG9L01VFg90wS",
+      "name": "summer",
+      "id": "RDCLAK5uy_mio8NKN0dPfEdyHxD9gDJK2iUYD4NELPk",
       "max": 100
-    },
-    {
-      "name": "popular-music-videos",
-      "id": "PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI",
-      "max": 200
-    },
-    {
-      "name": "top-hits-this-week",
-      "id": "PLw-VjHDlEOgvWPpRBs9FRGgJcKpDimTqf",
-      "max": 130
     }
 ];
